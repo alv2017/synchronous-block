b46fd69b85c440eb85226f7f83a40cd61ede6581
alv2017 <v.alishauskaite@gmail.com>
2025-10-27 16:51:04 +0200

    measure_response_time() function renamed to measure_get_response_time

--- measurements/performance/utils.py
+++ measurements/performance/utils.py
@@ -1,7 +1,7 @@
 from httpx import Client
 
 
-def measure_response_time(url: str) -> float:
+def measure_get_response_time(url: str) -> float:
     """
     Measures the response time of a given API endpoint.
 
@@ -13,5 +13,22 @@ def measure_response_time(url: str) -> float:
     """
     with Client() as client:
         response = client.get(url)
+        response.raise_for_status()
+        return response.elapsed.total_seconds()
+
+
+def measure_post_response_time(url: str, data: dict) -> float:
+    """
+    Measures the response time of a given API endpoint for POST requests.
+
+    Args:
+        url (str): The URL of the API endpoint to measure.
+        data (dict): The data to be sent in the POST request.
+
+    Returns:
+        float: The response time in seconds.
+    """
+    with Client() as client:
+        response = client.post(url, json=data)
         response.raise_for_status()
         return response.elapsed.total_seconds()
--- measurements/response_times/api_health_endpoint/response_time.py
+++ measurements/response_times/api_health_endpoint/response_time.py
@@ -6,10 +6,10 @@ sys.path.append(project_root)
 
 
 if __name__ == "__main__":
-    from measurements.performance import measure_response_time
+    from measurements.performance import measure_get_response_time
     N = 100  # Number of requests to send
     url = "http://127.0.0.1:8000/api/health/"
     total_time = 0.0
     for _ in range(100):
-        total_time += measure_response_time(url)
+        total_time += measure_get_response_time(url)
     print(f"Average response time over {N} requests: {total_time / N:.6f} seconds")
